11a7e51c8b143ba68f7d4986dc958e37fd031b0e
Bikrom-Roy <bikromroy2001@gmail.com>
2022-12-29 19:48:13 +0600

    update about

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,10 +22,51 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const postCollection = client.db("Echobooks").collection("post");
+    const userCollection = client.db("Echobooks").collection("userinfo");
 
     app.post("/post", async (req, res) => {
       const posts = req.body;
       const result = await postCollection.insertOne(posts);
+      res.send(result);
+    });
+
+    app.get("/userinfo", async (req, res) => {
+      const query = {};
+      const result = await userCollection
+        .find(query)
+        .sort({ like: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/post", async (req, res) => {
+      const query = {};
+      const result = await postCollection
+        .find(query)
+        .sort({ like: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/mediaDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await postCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/updateAboute/:id", async (req, res) => {
+      const id = req.params.id;
+      const user = req.body;
+      const query = { _id: ObjectId(id) };
+      const option = { upsert: true };
+      const updateDoc = {
+        $set: {
+          university: user.university,
+          address: user.address,
+        },
+      };
+      const result = await userCollection.updateOne(query, updateDoc, option);
       res.send(result);
     });
   } finally {
